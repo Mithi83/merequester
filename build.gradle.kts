8bef3da4a8a3103b86d1a2bda578ebc1e82288e5
rlnt <relentless@rlnt.dev>
2025-08-26 20:16:29 +0200

    jij the api dependency and avoid publishing to POM

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,7 +31,7 @@ repositories {
 
 dependencies {
     implementation("org.appliedenergistics:appliedenergistics2:${almostgradle.getProperty("aeVersion")}")
-    implementation("de.mari_023:ae2wtlib_api:${almostgradle.getProperty("wtlibVersion")}") { isTransitive = false }
+    jarJar(localImplementation("de.mari_023:ae2wtlib_api:${almostgradle.getProperty("wtlibVersion")}") { isTransitive = false })
 }
 
 tasks.withType<Jar> {
