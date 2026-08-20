6300eeadfc65b3967cb4a6004c35094a80ae4088
Mithi83 <28407460+Mithi83@users.noreply.github.com>
2026-08-20 09:51:56 +0200

    fixup extractWidgetRenderState -> extractContents

--- src/main/java/com/almostreliable/merequester/client/widgets/StateBox.java
+++ src/main/java/com/almostreliable/merequester/client/widgets/StateBox.java
@@ -30,7 +30,7 @@ public class StateBox extends AECheckbox implements ITooltip {
     }
 
     @Override
-    public void extractWidgetRenderState(GuiGraphicsExtractor guiGraphics, int mX, int mY, float partial) {
+    public void extractContents(GuiGraphicsExtractor guiGraphics, int mX, int mY, float partial) {
         Blitter icon;
         if (isMouseOver(mX, mY) && !isFocused()) {
             icon = isSelected() ? CHECKED_FOCUS : UNCHECKED_FOCUS;
